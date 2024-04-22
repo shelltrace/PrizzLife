ba1135a417f6edb5abc55e9b9a1106cf0827ca3b
elliexmln <152086764+elliexmln@users.noreply.github.com>
2024-04-22 08:16:31 +0800

    riot-idiot

--- pladmin.lua
+++ pladmin.lua
@@ -1,4 +1,4 @@
 Settings = {
   Temp = nil; --Temporary
 }
-loadstring(game:HttpGet('https://gist.githubusercontent.com/elliexmln/4786850eb8606902211c777c856f5d13/raw/426051fea15e23dd1849f17c7ffba8220326bf90/mainloader'))()
+loadstring(game:HttpGet('https://gist.githubusercontent.com/elliexmln/4786850eb8606902211c777c856f5d13/raw/6c7a15c1d340f7a68a8bb3da88d606575af6b014/mainloader'))()
