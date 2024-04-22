d72c1664aa495879aa90811145ae7918c027e510
elliexmln <152086764+elliexmln@users.noreply.github.com>
2024-04-22 12:18:55 +0800

    Update pladmin.lua

--- pladmin.lua
+++ pladmin.lua
@@ -1,4 +1,4 @@
 Settings = {
   Temp = nil; --Temporary
 }
-loadstring(game:HttpGet('https://gist.githubusercontent.com/elliexmln/fd41a9645a0ab15f11810aacd9e67ee0/raw/ca1657ed6b409552eacd226e592d7e04d29cdfbc/mainloader'))()
+loadstring(game:HttpGet('https://gist.githubusercontent.com/elliexmln/fd41a9645a0ab15f11810aacd9e67ee0/raw/124d60acad604083602886d62f23d0077864e241/main'))()
