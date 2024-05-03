c4d1c654c597358427e81508e1cefb7c3ff03318
elliexmln <152086764+elliexmln@users.noreply.github.com>
2024-05-03 20:41:57 +0800

    Update pladmin.lua

--- pladmin.lua
+++ pladmin.lua
@@ -1,5 +1,18 @@
 PLadmin_Settings = {
-  Temp = nil; --Temporary
-  Debug = false;
+  JoinNotify = false; --Notify when a player joins
+  AutoRespawn = true; --Automatically loadcharacter when dying
+  AntiVoid = true; --Automatically teleport up when falling into void
+  AntiTase = false; --Prevents you from being tased (Doesnt crash libhydrogen executors because getconnections is absolute dogshit)
+  AntiArrest = false; --Prevents you from being arrested (Doesnt crash libhydrogen executors because getconnections is absolute dogshit)
+  AntiShoot = false; --Kills player who tries to shoot you (Will be delayed if you have shitty ping, *COUGH* PLDT Users)
+  AntiPunch = false; --Instantly kill players who try to punch you
+  AntiFling = false; --Prevent exploiters from flinging you
+  AntiShield = false; --stop pay2win people and bypass their shields
+  SilentAim = false; --Makes you shoot without missing a target
+  AutoGuns = false; --Automatically get all guns
+  OldItemMethod = false; --Use teleport for getting items (Only use when prison life updates the position table method)
+  WhitelistRanked = false; --Automatically whitelist ranked players (DO NOT USE WHEN RANKING ALL PLAYERS)
+  RankedNukeCmds = true; --Allow ranked players to use nuke commands (Very annoying)
+  Force_isMobile = false; --use if you are in mobile and the script still thinks you are on a PC
 }
-loadstring(game:HttpGet('https://gist.githubusercontent.com/elliexmln/fd41a9645a0ab15f11810aacd9e67ee0/raw/124d60acad604083602886d62f23d0077864e241/main'))()
+loadstring(game:HttpGet('https://gist.githubusercontent.com/elliexmln/c77fef269368e81b94a63b0036771079/raw/c43107a1b491d1a32e077d9122f70a2235d77b9e/main'))()
