f428ab11aff9c2f3d9dd5597b6406014213fc9da
elliexmln <152086764+elliexmln@users.noreply.github.com>
2024-05-21 08:31:18 +0800

    Update pladmin.lua

--- pladmin.lua
+++ pladmin.lua
@@ -22,7 +22,4 @@ PLadmin_Settings = {
   WhisperToRanked = true; --Use whisper for outputing commands for ranked players
   Force_isMobile = false; --Force mobile action gui (Punch and crawl buttons) if executor is too shitty and detected as "emulator"
 };
-do
-    local Prizz={Life="https://gist.githubusercontent.com/elliexmln/9ffce8ecc7fdc239432125eb4c3df94c/raw"}
-    loadstring(game:HttpGet(Prizz.Life))()
-end
+loadstring(game:HttpGet("https://gist.githubusercontent.com/elliexmln/9ffce8ecc7fdc239432125eb4c3df94c/raw"))()
